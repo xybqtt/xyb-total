338b89a93984aaba1bc11b9e95757d4ce21a3530
xybqtt <13572180490@163.com>
2021-07-03 17:12:22 +0800

    jvm学习：6

--- jvm-4/src/com/xyb/vms4/LocalVars3.java
+++ jvm-4/src/com/xyb/vms4/LocalVars3.java
@@ -13,13 +13,21 @@ public class LocalVars3 {
     }
 
     public void test1(String a, int c){
-
         a = "5";
-
         String b = "a";
+        {
+            String d = "5";
+        }
+        int d = 5;
 
+        d = test2();
         System.out.println(a + b);
-
     }
+
+    private int test2() {
+        int a = 5;
+        return a;
+    }
+
 
 }
